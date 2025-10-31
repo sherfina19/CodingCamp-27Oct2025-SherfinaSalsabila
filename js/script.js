be7972ccf6859b44f871c05981fd63ca5141c58f
sherfina19 <sherfina19@gmail.com>
2025-11-01 02:11:00 +0700

    update: const quotes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+const quotes = [
+  "It’s not about being the best. It’s about being better than you were yesterday.",
+  "One bug at a time, one step closer to perfection.",
+  "Tiny progress is still progress.",
+  "You got this, even the stars started small.",
+  "Be kind to yourself. You’re learning.",
+  "Don’t be afraid to fail, be afraid to never try.",
+  "Strong mind, soft heart, steady hands.",
+  "Every step forward, no matter how small, matters.",
+  "Keep shining softly, your light’s enough.",
+  "Hey, don’t rush. You’re growing beautifully."
+];
+
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+document.getElementById('motivation').textContent = `"${randomQuote}"`;
 const addBtn = document.getElementById('addBtn');
 const taskInput = document.getElementById('taskInput');
 const dateInput = document.getElementById('dateInput');
